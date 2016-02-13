90cd987e588ae889c3c70b62f1d660bdafec26ba
jdlawrence <jamil.lawrence@gmail.com>
2016-02-13 14:22:24 -0800

    Updated with gitignore

--- server.js
+++ server.js
@@ -4,8 +4,7 @@ var bodyParser = require('body-parser');
 var app = express();
 
 app.use(express.static(path.join(__dirname, 'client')));
-app.set('port', (process.env.PORT || 3500));
-// app.listen(process.env.PORT || 3500);
+app.set('port', (process.env.PORT || 4000));
 
 app.listen(app.get('port'), function() {
   console.log('Server started: http://localhost:' + app.get('port') + '/');
